e019ccf7e55b48c732a8f4b5cea947fc69f487c9
Your Name <you@example.com>
2023-09-01 03:12:30 +0600

    first

---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const blogBtn = document.getElementById("blog-btn");
+blogBtn.addEventListener("click", function () {
+    window.location.href ='blog.html'
+});
